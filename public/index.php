2e7461dcf5b55a29a63b01b332bc8e487ede7e5f
= <=>
2021-04-01 21:55:11 +0200

    Whoops error handler

--- public/index.php
+++ public/index.php
@@ -1,5 +1,24 @@
 <?php
 
-require dirname(__DIR__).'/config/bootstrap.php';
+/**
+ * Self-called anonymous function that creates its own scope and keep the global namespace clean.
+ */
+call_user_func(function () {
+    require dirname(__DIR__) . '/config/bootstrap.php';
 
-die("Blog app");
+    $environment = 'development';
+    /**
+     * Register the error handler
+     */
+    $whoops = new \Whoops\Run;
+    if ($environment !== 'production') {
+        $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+    } else {
+        $whoops->pushHandler(function($e){
+            echo 'Something went wrong ;(';
+        });
+    }
+    $whoops->register();
+
+    die("Blog app");
+});
